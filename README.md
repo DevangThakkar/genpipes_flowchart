11251a8a43fcf2631252b72cd3d0fdd0003d75d7
Devang <devang01121995@gmail.com>
2018-05-24 21:13:15 +0530

    add optparse functionality

---
+++ README.md
@@ -0,0 +1,44 @@
+# Automating Flowchart creation for GenPipes pipelines
+
+This project allows for automatic creation of flowcharts whenever a GenPipes pipeline is executed, either partially or wholly. The only requisite for this script to run is a hierarchy file that lists down the relationship between the steps. The format for the hierarchy file is as follows:
+
+The predecessor(s) for each node are described using a certain set of schema. The numbers refer to the steps indicated by the number whereas the term "DATA" refers to external data being used. There are two types of connectors relating the predecessors for a certain node:
+
+ * ",": The node considers exactly one of the mentioned predecessors. 
+
+Example: Step 2:trimmomatic needs exactly one of either (i) the FASTQ files or (ii) the converted FASTQ files from step 1, with (i) preferred over (ii).
+
+* "+": The node needs at least one of the predecessors but considers multiple predecessors, if present.
+
+Example: Step 9:picard_mark_duplicates considers as input, all of the output files created in step 5, 6, and 8 - at least one of them being necessary.
+
+Notes:
+* Any line beginning with a hash (#) is considered to be a comment.
+* Do not use spaces/tabs between the step number and step name
+
+## Sample from hierarchy_dnaseq.tsv, the linkage document for the dnaseq pipeline:
+
+BAM							1:picard_sam_to_fastq				(1) uses BAM data
+FASTQ,1						2:trimmomatic						(2) uses FASTQ data if available, else (1)
+2							3:merge_trimmomatic_stats			(3) uses (2)
+3,FASTQ,1					4:bwa_mem_picard_sort_sam			(4) uses (3) if available, else FASTQ if available, else (1)
+4,BAM						5:picard_merge_sam_files			(5) uses (4) if available, else BAM
+5							6:gatk_indel_realigner				(6) uses (5)
+6							7:merge_realigned					(7) uses (6)
+6							8:fix_mate_by_coordinate			(8) uses (6)
+5+6+8						9:picard_mark_duplicates			(9) uses as many of (5), (6), and (8) as available
+
+
+## Usage:
+
+usage: flowchart.py [-h] --steps STEPS --h_file H_FILE [--bam BAM]
+                    [--fastq FASTQ]
+
+Creating flowcharts for GenPipe pipeline executions
+
+optional arguments:
+  -h, --help       show this help message and exit
+  --steps STEPS    step range e.g. "1-5", "3,6,7", "2,4-8"
+  --h_file H_FILE  path to hierarchy file for pipeline
+  --bam BAM        mention if SAM/BAM data is present in READSET
+  --fastq FASTQ    mention if FASTQ is present in READSET
--- flowchart.py
+++ flowchart.py
@@ -23,6 +23,7 @@
 # PEP-8 format: Limit all lines to a maximum of 79 characters ----------------|
 
 # Python Standard Modules
+import argparse
 import os
 import re
 import sys
@@ -47,9 +48,32 @@ class FlowChart():
 
 	"""
 
-	def __init__(self, steps, hierarchy_file_name):
-		self.parse_steps(steps)
+	def __init__(self, steps, hierarchy_file_name, if_bam, if_fastq):
+		"""
+		Parameters
+		----------
+		steps: string
+			Steps of the pipeline being run
+		hierarchy_file_name: string
+			Path to hierarchy_file
+		if_bam: boolean
+			Stores if READSET has BAM data or not
+		if_fastq: boolean
+			Stores if READSET has FASTQ data or not
+
+		Modifies
+		----------
+		self.if_bam: boolean
+			Stores if READSET has BAM data or not
+		self.if_fastq: boolean
+			Stores if READSET has FASTQ data or not
+		"""
+
+		self.if_bam = if_bam
+		self.if_fastq = if_fastq
+
 		self.parse_hierarchy(hierarchy_file_name)
+		self.parse_steps(steps)
 		self.create_flowchart(self.check_validity())
 
 	def parse_steps(self, steps):
@@ -70,30 +94,60 @@ class FlowChart():
 
 		Modifies
 		----------
+		self.steps: string
+			String passed to the function
 		self.step_list: list
 			List of steps being executed
 		"""
 
+		if steps == "":
+			print("Error: STEPS can not be empty. Please try again.")
+			sys.exit(0)
 		self.steps = steps
 		self.step_list = []
+
 		comma_delimited_steps = steps.strip().split(",")
 		for step in comma_delimited_steps:
 			if "-" in step:
-				low = int(step.strip().split("-")[0])
-				high = int(step.strip().split("-")[1])
+				low = step.strip().split("-")[0]
+				high = step.strip().split("-")[1]
+				
+				# if either of high or low is empty -> negative integer, exit
+				if low == "" or high == "":
+					print("Error: The steps entered seem to be incorrect.")
+					sys.exit(0)
+
+				low = int(low)
+				high = int(high)
+
+				# if there's a zero, exit
+				if low <= 0:
+					print("Error: steps start from 1, not 0.")
+					sys.exit(0)
+				
 				for i in range(low, high + 1):
 					self.step_list.append(str(i))
 			else:
+				# if there's a zero, exit
+				if int(step.strip()) <= 0:
+					print("Error: steps start from 1, not 0.")
+					sys.exit(0)
+
 				self.step_list.append(step.strip())
 
 		# sort elements of step as if they were integers
 		def natural_key(string_):
 			return [int(s) if s.isdigit() else s
-					for s in re.split(r'(\d+)', string_)]
+					for s in re.split(r"(\d+)", string_)]
 
 		self.step_list = list(set(self.step_list))
 
 		self.step_list.sort(key=natural_key)
+
+		# ensure all steps in STEP are <= self.max_step
+		if int(self.step_list[len(self.step_list)-1]) > self.max_step:
+			print("Error: You're trying to access a step which doesn't exist.")
+			sys.exit(0)
 
 	def parse_hierarchy(self, hierarchy_file_name):
 		"""
@@ -111,11 +165,16 @@ class FlowChart():
 			Dictionary of links between steps
 		self.name_list: list
 			List of names of steps
-
+		self.hierarchy_file: string
+			Stores the path to the hierarchy_file
+		self.max_step: int
+			Stores the id of the final step in the pipeline
 		"""
+
 		self.links = dict()
 		self.name_list = []
 		self.hierarchy_file = hierarchy_file_name
+		self.max_step = 1
 
 		with open(hierarchy_file_name, "r") as f:
 			for line in f:
@@ -133,6 +192,10 @@ class FlowChart():
 					step_name = step_term.split(":")[1]
 					self.name_list.append(step_name)
 					self.links[step_number] = predecessor_term
+					
+					#identify the final step
+					if int(step_number) > self.max_step:
+						self.max_step = int(step_number)
 
 	def check_validity(self):
 		"""
@@ -149,12 +212,15 @@ class FlowChart():
 		# make a copy of the list of steps
 		temp_list = self.step_list[:]
 
-		# add data to the copy of list of steps to account for top nodes
-		temp_list.append("DATA")
+		# add BAM/FASTQ to the copy of list of steps to account for top nodes
+		if self.if_bam:
+			temp_list.append("BAM")
+		if self.if_fastq:
+			temp_list.append("FASTQ")
 
 		for step in temp_list:
-			# ignore the validation for dummy value "DATA"
-			if step == "DATA":
+			# ignore the validation for dummy values BAM and FASTQ
+			if step == "BAM" or step == "FASTQ":
 				continue
 
 			# pred = predecessor(s) of step
@@ -199,14 +265,15 @@ class FlowChart():
 
 		"""
 
-		# if graph is disconnected, create dummy file and exit
+		# if graph is erroneous, create dummy file and exit
 		if not verity:
 
 			# create a graph using Graphviz
-			dot = Digraph(comment='Flowchart', node_attr={'shape': 'plaintext'})
+			dot = Digraph(comment="Flowchart",
+							node_attr={"shape": "plaintext"})
 
 			# add error node
-			dot.node("0", "Graph not created as some nodes are disconnected")
+			dot.node("0", "Graph not created: some nodes don't have a source")
 
 			try:
 				# create folder if not exists
@@ -214,13 +281,23 @@ class FlowChart():
 				if not os.path.exists(dir_name):
 					os.makedirs(dir_name)
 
-				# save graph
-				dot.render(dir_name + self.hierarchy_file + "-" + self.steps +
-							".error")
+				if self.if_bam and self.if_fastq:
+					dot.render(dir_name + self.hierarchy_file + "-" +
+								self.steps + ".bam.fastq.error")
+
+				if self.if_bam and not self.if_fastq:
+					dot.render(dir_name + self.hierarchy_file + "-" +
+								self.steps + ".bam.error")
+
+				if not self.if_bam and self.if_fastq:
+					dot.render(dir_name + self.hierarchy_file + "-" +
+								self.steps + ".fastq.error")
 			except Exception as inst:
+
 				# raise exception if file is open and can not be modified
 				print("The target file seems to be open already. Please" +
 						" close the file before proceeding.")
+
 
 			return
 
@@ -230,21 +307,23 @@ class FlowChart():
 		# store all the edges that have been added so far
 		added_tuples = []
 
-		# hard code the "DATA" node
-		data_name = "DATA"
-		added_nodes.append(data_name)
-
-		# creating a copy of self.step_list with "DATA" in it
+		# creating a copy of self.step_list with BAM/FASTQ in it
 		temp_list = self.step_list[:]
-		temp_list.append(data_name)
+
+		# hard code the BAM, FASTQ node if present
+		if self.if_bam:
+			added_nodes.append("BAM")
+			temp_list.append("BAM")
+		if self.if_fastq:
+			added_nodes.append("FASTQ")
+			temp_list.append("FASTQ")
 
 		for step in temp_list:
-			if step == data_name:
+			if step == "BAM" or step == "FASTQ":
 				continue
 
 			# if step has only one predecessor
 			if "," not in self.links[step] and "+" not in self.links[step]:
-
 				# if step is not added, add a node for step
 				if step not in added_nodes:
 					added_nodes.append(step)
@@ -299,20 +378,22 @@ class FlowChart():
 							added_nodes.append(item)
 
 		# create a graph using Graphviz
-		dot = Digraph(comment='Flowchart', node_attr={'shape': 'rectangle'})
+		dot = Digraph(comment="Flowchart", node_attr={"shape": "rectangle"})
 
 		# add nodes
 		for i in added_nodes:
-			if i == "DATA":
-				dot.node(i, "READSET DATA")
-			else:
+			if i == "BAM":
+				dot.node(i, "BAM")
+			if i == "FASTQ":
+				dot.node(i, "FASTQ")
+			if i != "BAM" and i != "FASTQ":
 				dot.node(i, i + ":" + self.name_list[int(i)-1])
 
 		# add edges
 		for (i, j) in added_tuples:
 			dot.edge(i, j)
 
-		dot.edge_attr.update(arrowhead='normal')
+		dot.edge_attr.update(arrowhead="normal")
 
 		try:
 			# create folder if not exists
@@ -321,8 +402,17 @@ class FlowChart():
 				os.makedirs(dir_name)
 
 			# save graph
-			dot.render(dir_name + self.hierarchy_file + "-" + self.steps +
-						".txt")
+			if self.if_bam and self.if_fastq:
+				dot.render(dir_name + self.hierarchy_file + "-" + self.steps +
+							".bam.fastq.flow")
+
+			if self.if_bam and not self.if_fastq:
+				dot.render(dir_name + self.hierarchy_file + "-" + self.steps +
+							".bam.flow")
+
+			if not self.if_bam and self.if_fastq:
+				dot.render(dir_name + self.hierarchy_file + "-" + self.steps +
+							".fastq.flow")
 		except Exception as inst:
 			# raise exception if file is open and can not be modified
 			print("The target file seems to be open already. Please" +
@@ -330,4 +420,45 @@ class FlowChart():
 
 
 if __name__ == "__main__":
-	FlowChart("5-10,17-20", "hierarchy_dnaseq.tsv")
+
+	# description of parser
+	desc_string = "Creating flowcharts for GenPipe pipeline executions"
+	parser = argparse.ArgumentParser(description=desc_string)
+	
+	# add compulsory argument steps
+	parser.add_argument("--steps", nargs=1, required=True,
+						help="step range e.g. \"1-5\", \"3,6,7\", \"2,4-8\"")
+
+	# add compulsory argument hierarchy file
+	parser.add_argument("--h_file", nargs=1, required=True,
+						help="path to hierarchy file for pipeline")
+
+	# function to accept and convert values for --bam and --fastq
+	def str2bool(v):
+		if v.lower() in ("yes", "true", "t", "y", "1"):
+			return True
+		elif v.lower() in ("no", "false", "f", "n", "0"):
+			return False
+		else:
+			raise argparse.ArgumentTypeError("Boolean value expected.")
+
+	# add optional argument bam
+	parser.add_argument("--bam", type=str2bool, nargs=1, default=True,
+						help="mention if SAM/BAM data is present in READSET")
+
+	# add optional argument fastq
+	parser.add_argument("--fastq", type=str2bool, nargs=1, default=True,
+						help="mention if FASTQ is present in READSET")
+
+	args = parser.parse_args()
+
+	# convert args to usable format
+	steps = repr(args.steps).replace("['", "").replace("']", "")
+	h_file = repr(args.h_file).replace("['", "").replace("']", "")
+	bam = repr(args.bam).replace("[", "").replace("]", "")
+	fastq = repr(args.fastq).replace("[", "").replace("]", "")
+	bam = True if bam == "True" else False
+	fastq = True if fastq == "True" else False
+	
+	# start the program logic
+	FlowChart(steps, h_file, bam, fastq)
